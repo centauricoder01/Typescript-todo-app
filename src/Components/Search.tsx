25fecca8a6eece8a02089e18b7d402817b36b771
Rajendra <itsrajendrapatel@gmail.com>
2023-07-19 12:55:12 +0530

    Necessary files installed and searchbar created

---
+++ src/Components/Search.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Search = () => {
+
+    interface StringObject {
+        [key: string]: string;
+    }
+    const formStyle: StringObject = {
+        width: "50%",
+        margin: "auto",
+        display: "flex",
+        justifyContent: "center",
+        alignItem: "center",
+        padding: "1rem",
+        gap: "1rem"
+    }
+
+    const inputStyle: StringObject = {
+        width: "85%",
+        height: "2.5rem",
+        borderRadius: "5px",
+        border: "none",
+        paddingLeft: "1rem"
+    }
+
+    const buttonStyle: StringObject = {
+        width: "15%",
+        height: "2.5rem",
+        border: "none",
+        borderRadius: "2px",
+        cursor: "pointer"
+    }
+    return (
+        <>
+            <form style={formStyle}>
+                <input type="text" style={inputStyle} placeholder='Enter Your task' />
+                <button style={buttonStyle}>Add Task.</button>
+            </form>
+        </>
+    )
+}
+
+export default Search
